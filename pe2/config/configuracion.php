8de53ac6a8578832af4658403cb493e3a7504ae6
FabriConde <fabricon20@gmail.com>
2026-06-07 13:11:16 +0200

    conexesion base datps

--- pe2/config/configuracion.php
+++ pe2/config/configuracion.php
@@ -1,7 +1,7 @@
 <?php
     define("DB_DSN", "mysql:host=localhost;dbname=dbfabriconde_pw2526" );
-    define("DB_USUARIO", "root" );
-    define("DB_CONTRASENIA", "" );
+    define("DB_USUARIO", "pwfabriconde" );
+    define("DB_CONTRASENIA", "25fabriconde26" );
     define("USUARIOS", "usuarios" );
     define("VIAJES", "viajes" );
-?>
+?>    
